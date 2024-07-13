e126b35449319b132879f98e2a2909e3c0f50395
Elward Crianza <ecrianza@excelliscorp.com>
2024-07-13 22:07:18 +0800

    Add project files.

---
+++ Controllers/PuppiesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace adopt_a_puppy_aspnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PuppiesController : ControllerBase
+    {
+    }
+}
